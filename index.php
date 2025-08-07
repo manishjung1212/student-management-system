acc4c8d44f33390cf5c53b0f218c203e36bc9d71
manishjung1212 <shahimanish238@gmail.com>
2025-08-07 10:30:54 +0400

    make folder

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+</head>
+<h1>hello<h1>
+<body>
+    <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
